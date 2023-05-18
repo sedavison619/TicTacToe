34e22985e7fc33fda3a64135d4e152aaf2a3c826
Sarah Davison <sarah.davison619@gmail.com>
2023-05-17 21:54:41 -0500

    added new game and partial reset functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+class Gameboard{
+    constructor (emptySpaces, xPlayer, oPlayer, currentPlayer){
+        //creates an array of empty elements for the game spaces
+        emptySpaces = Array.from(document.getElementsByClassName('square'))
+        xPlayer = 'x'
+        oPlayer = 'o'
+        currentPlayer = xPlayer
+    }
+    new(){       
+        //creates a new gameboard
+        for (let i = 0; i < 9; i++){
+            const oneSpace = document.createElement('section');
+            oneSpace.classList.add('square');
+            oneSpace.id = i;
+            document.getElementById('gameboard').appendChild(oneSpace)
+        }
+    }
+    clear(){
+        //resets player marker
+        this.currentPlayer = xPlayer
+        //resets 
+        squares.fill(null)
+        
+    }
+}
 
+const ticTacToe = new Gameboard
+
+document.querySelector('button').addEventListener('click', ticTacToe.clear)
+
+ticTacToe.new()
